6d1b99e62f64345b4da31ba973eec235f6c439cd
Mateo Tiedra <mateo@tiedra.com>
2025-03-02 16:53:12 +0100

    finish text and implement footer in about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,22 +1,23 @@
+import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
 import { useGlitch } from 'react-powerglitch';
 
 const About = () => {
   const assocText = (
     <>
-      Fondée le 9 octobre 2023, <strong>LÜMM</strong> est une association à but{' '}
-      <strong>non lucratif</strong> qui a pour mission de rendre la culture{' '}
-      <strong>accessible</strong> tout en mettant en lumière des artistes{' '}
-      <strong>émergents</strong>.{<br />}
-      Les buts de l&apos;association incluent l&apos;organisation
-      d&apos;événements hyper accessibles avec des prix très bas ou libres,
-      ainsi que le soutien et la mise en lumière des artistes dans les domaines
-      de la <strong>musique</strong>, de la <strong>mode</strong> et des{' '}
-      <strong>arts visuels</strong> qui partagent ses valeurs.{<br />}Par
-      ailleurs, elle s&apos;efforce de développer et de diffuser une identité
-      audiovisuelle unique et accessible, contribuant ainsi à enrichir le
-      paysage artistique, tout en encourageant la créativité et
-      l&apos;inclusivité dans les domaines de l’art et de l’audiovisuel.
+      Fondée le 9 octobre 2023 par 7 étudiants, <strong>LÜMM</strong> est une
+      association à but <strong>non lucratif</strong> qui a pour mission de
+      rendre la culture <strong>accessible</strong> tout en mettant en lumière
+      des artistes <strong>émergents</strong>.{<br />}
+      Les <strong>buts</strong> de l’association incluent l’organisation
+      d’événements <strong>hyper accessibles</strong> avec des prix très bas ou
+      libres, ainsi que le soutien et la mise en lumière des{' '}
+      <strong>artistes</strong> dans les domaines de la <strong>musique</strong>
+      , de la <strong>mode</strong> et des <strong>arts visuels</strong>.
+      {<br />}Par ailleurs, elle s’efforce de développer et de diffuser une
+      identité <strong>audiovisuelle unique</strong>, contribuant ainsi à
+      enrichir le paysage artistique, tout en encourageant la créativité et
+      l’inclusivité.
     </>
   );
 
@@ -43,24 +44,29 @@ const About = () => {
   return (
     <>
       <Navbar />
-      <div className='flex flex-col items-center md:hidden section-container first-section '>
+      {/* Mobile */}
+      <div className='flex flex-col items-center md:hidden section-container first-section overflow-hidden'>
         <img
           src='/images/comite.jpg'
           className='max-w-sm mt-[10vh] mb-8'
           ref={glitch.ref}
         />
-        <p className='text-center text-md'>{assocText}</p>
+        <h1 className='pb mr-auto'>- Notre histoire</h1>
+        <p className='text-start text-md'>{assocText}</p>
       </div>
-      <div className='hidden md:flex w-full flex-row items-center space-between overflow-hidden'>
+      {/* Desktop */}
+      <div className='hidden md:flex w-screen h-screen flex-row items-center space-between overflow-hidden relative'>
         <img
           src='/images/comite.jpg'
-          className='h-screen absolute -top-5 -left-20 block'
+          className='h-screen absolute -top-5 -right-[calc(200px-10vw)] block'
           ref={glitch.ref}
         />
-        <p className=' absolute top-1/2 left-1/2 -translate-1/2 text-center text-md w-2/3 max-w-[620px] bg-black px-10 py-6'>
-          {assocText}
-        </p>
+        <div className='flex flex-col items-start absolute top-1/2 left-2/5 -translate-1/2 w-2/3 max-w-[650px] bg-black px-10 py-6'>
+          <h1 className='pb'>- Notre histoire</h1>
+          <p className='text-start text-md'>{assocText}</p>
+        </div>
       </div>
+      <Footer />
     </>
   );
 };
