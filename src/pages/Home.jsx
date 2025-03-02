70c33b54bd686f44a66dda88950ebcd55cb26c7d
Mateo Tiedra <mateo@tiedra.com>
2025-03-02 23:51:42 +0100

    add footer to home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,23 @@
 //import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
 
 const Home = () => {
   return (
-    <div className='h-screen w-full flex-centered'>
-      <Navbar />
-      <video
-        autoPlay
-        loop
-        muted
-        className='h-full min-w-[600px] w-2/3 object-contain'
-      >
-        <source src='/public/videos/anim-logo-home.mp4' type='video/mp4' />
-      </video>
-    </div>
+    <>
+      <Navbar over />
+      <div className='h-screen w-full flex-centered'>
+        <video
+          autoPlay
+          loop
+          muted
+          className='h-full min-w-[600px] w-2/3 object-contain'
+        >
+          <source src='/public/videos/anim-logo-home.mp4' type='video/mp4' />
+        </video>
+      </div>
+      <Footer />
+    </>
   );
 };
 
